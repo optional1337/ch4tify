69db40d92e3231d7abfbd8ef0185a165d89898bc
optional1337 <sgourav12121212@gmail.com>
2025-04-30 22:47:58 +0530

    folder structure correction...

--- Backend/server.js
+++ Backend/server.js
@@ -32,10 +32,10 @@ app.use("/api/messages", messageRoutes);
 
 
 
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
   
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
     });
   
 server.listen(PORT, () => {
