bf9b13d5754616e818ba1c51c010c32159d74c1a
optional1337 <sgourav12121212@gmail.com>
2025-04-30 22:40:59 +0530

    folder structure correction...

--- Backend/server.js
+++ Backend/server.js
@@ -31,13 +31,12 @@ app.use("/api/groups", groupRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
+
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-  }
   
 server.listen(PORT, () => {
     connectDB();
