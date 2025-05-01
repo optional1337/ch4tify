7fefd5aa51412ae584c307963c6cfb924e72ad3f
optional1337 <sgourav12121212@gmail.com>
2025-05-01 10:11:04 +0530

    final push...

--- Backend/controllers/auth-controller.js
+++ Backend/controllers/auth-controller.js
@@ -239,7 +239,7 @@ export const forgotPassword = async (req, res) => {
 
         // send email
 
-        // await sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}`);
+        await sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}`);
         
         res.status(200).json({success:true, message: "Reset password email sent successfully"});
     } catch (error) {
